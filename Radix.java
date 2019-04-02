c6a951e1eae357bd3197c8963a6e0f731a92aa9f
Tyler Huang <thuang00@stuy.edu>
2019-04-02 19:37:31 -0400

    working on radix

--- Radix.java
+++ Radix.java
@@ -1,8 +1,9 @@
 public class Radix{
   public static void radixsort(int[] data){
+    @SuppressWarnings({"rawtypes", "unchecked"})
     MyLinkedList<Integer>[] buckets = new MyLinkedList[20];
     for (int i =0; i < 20; i++){
-      buckets[i] = new MyLinkedList<Integer>(); 
+      buckets[i] = new MyLinkedList<Integer>();
     }
     int count = getMax(data);
     int base = 10;
@@ -15,6 +16,7 @@ public class Radix{
         }
       }
       int digit = data[idx] % base;
+
       if (data[i] >= 0){
         buckets[digit + 10].add(data[i]);
       }
@@ -25,9 +27,9 @@ public class Radix{
   }
   //find max of the data and return how many passes radix sort will need
   private static int getMax(int[] data){
-    int max = 0;
-    for (int i = 0; i < data.length; i++){
-      if (data[i] > max) max = data[i];
+    int max = data[0];
+    for (int i : data){
+      if (i > max) max = i;
     }
     int count = 1;
     while (max >= 10){
@@ -35,5 +37,9 @@ public class Radix{
       count++;
     }
     return count;
+  }
+  public static void main(String[] args){
+    //int[] arr = new int[]{12,199,13334};
+    //System.out.println(getMax(arr));
   }
 }
