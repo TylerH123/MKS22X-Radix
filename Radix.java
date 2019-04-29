106dbf066aefbc4e56c78fd7cf8cee24e5ac71da
Tyler Huang <thuang00@stuy.edu>
2019-04-29 18:52:34 -0400

    fixed for empty array

--- Radix.java
+++ Radix.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Radix{
   public static void radixsort(int[] data){
-    if (data.length == 0) return; 
+    if (data.length == 0) return;
     @SuppressWarnings({"rawtypes", "unchecked"})
     MyLinkedList<Integer>[] buckets = new MyLinkedList[20];
     for (int i =0; i < 20; i++){
@@ -102,42 +102,6 @@ public class Radix{
       output += data[i].toString() + " ";
     }
     return output;
-  }
-  public static void test(){
-    //int[] arr = new int[]{12,199,13334};
-    //System.out.println(getMax(arr));
-    System.out.println("Size\t\tMax Value\tquick/builtin ratio ");
-    int[]MAX_LIST = {1000000000,500,10};
-    for(int MAX : MAX_LIST){
-      for(int size = 31250; size < 2000001; size*=2){
-        long qtime=0;
-        long btime=0;
-        //average of 5 sorts.
-        for(int trial = 0 ; trial <=5; trial++){
-          int []data1 = new int[size];
-          int []data2 = new int[size];
-          for(int i = 0; i < data1.length; i++){
-            data1[i] = (int)(Math.random()*MAX);
-            data2[i] = data1[i];
-          }
-          long t1,t2;
-          t1 = System.currentTimeMillis();
-          radixsort(data2);
-          t2 = System.currentTimeMillis();
-          qtime += t2 - t1;
-          t1 = System.currentTimeMillis();
-          Arrays.sort(data1);
-          t2 = System.currentTimeMillis();
-          btime+= t2 - t1;
-          if(!Arrays.equals(data1,data2)){
-            System.out.println("FAIL TO SORT!");
-            System.exit(0);
-          }
-        }
-        System.out.println(size +"\t\t"+MAX+"\t"+1.0*qtime/btime);
-      }
-      System.out.println();
-    }
   }
   public static void main(String[] args){
     int[] arr = new int[]{100,1124,12412,91,32,134};
